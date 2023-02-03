83149af49e506e49dabb1fe472b1d9dbae429608
Blake Hourigan <houriganblake@protonmail.com>
2023-02-03 11:23:59 -0500

    first commit

---
+++ blakehourigan.com/src/App.js
@@ -0,0 +1,27 @@
+import { useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import {Button,Container} from '@mui/material';
+import {ThemeProvider} from '@emotion/react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">
+        <Button>test</Button>
+        <img src="https://i.imgur.com/06otQtL.jpg" className="App-logo" alt="logo" />
+        <p>
+          Under Construction
+        </p>
+        
+
+      </header>      
+
+    </div>
+    
+
+  );
+}
+
+export default App;
