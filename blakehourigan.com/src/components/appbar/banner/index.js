f893575e35382ef80ed0eba9a7d020ca1b915c13
blakehourigan <houriganblake@protonmail.com>
2023-02-06 15:39:24 -0500

    les goooooo

--- blakehourigan.com/src/components/appbar/banner/index.js
+++ blakehourigan.com/src/components/appbar/banner/index.js
@@ -1,5 +1,6 @@
-import { useMediaQuery } from "@mui/material";
+import { Typography, useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import { BannerContainer,BannerContent, BannerDescription, BannerImage, BannerTitle } from "../../../styles/banner";
 
 
 export default function Banner() {
@@ -7,6 +8,16 @@ export default function Banner() {
     const matches = useMediaQuery(theme.breakpoints.down('md'));
     
     return (
-        <h1>Banner</h1>
+        <BannerContainer>
+                <BannerContent>
+                    <BannerImage src="https://i.imgur.com/06otQtL.jpg"/>
+                <Typography variant="h6">testing</Typography>
+                <BannerTitle variant="h2">
+                    testing v2
+                </BannerTitle>
+                <BannerDescription variant="subtitle">testing v3</BannerDescription>
+            </BannerContent>
+        </BannerContainer>
+        
     );
 }
